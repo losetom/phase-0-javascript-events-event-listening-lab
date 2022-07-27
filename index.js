658adec3c0dc4a3c02f1412ae2b1f990d69abc4f
Mike Loseto <losetom1998@gmail.com>
2022-07-27 13:27:25 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+
+
+    // const input = document.getElementById('input');
+    // if(input){
+    //     input.addEventListener('click', clickAlert, false); 
+    
+    // function clickAlert() {
+    //   alert('I was clicked!');
+    // }} 
+
+
